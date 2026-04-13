e816c0035759beaf528c13867da76697a40a3aba
pejuangrupiah009 <pejuangrupiahredmi@gmail.com>
2026-04-13 19:38:53 +0700

    Add files via upload

---
+++ login.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Login</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<form class="login-form">
+  <input type="text" placeholder="Username">
+  <input type="password" placeholder="Password">
+  <button>LOGIN</button>
+</form>
+
+</body>
+</html>
